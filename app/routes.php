25cb74be09153e2fc8c5bc956ee3ba11fa890add
Jamie Shepherd <jamie.shepherd@outlook.com>
2014-06-04 14:01:39 +0100

    Inline navigation for admin, looks all good in the hood :thumbsup:

--- app/routes.php
+++ app/routes.php
@@ -15,22 +15,27 @@ Route::get('/', function()
 {
     return View::make('index');
 });
+
 Route::get('/blog', function()
 {
     return View::make('blog');
 });
+
 Route::get('/tutorials', function()
 {
     return View::make('tutorials');
 });
+
 Route::get('/projects', function()
 {
     return View::make('projects');
 });
+
 Route::get('/contact', function()
 {
     return View::make('contact');
 });
+
 Route::get('/article', function()
 {
     return View::make('article');
@@ -42,14 +47,31 @@ Route::get('/login', function()
 {
     return View::make('login');
 });
+
+Route::post('/login', 'UserController@processLogin');
+
 Route::get('/logout', function()
 {
     Auth::logout();
     return View::make('login')->with('message', 'You have successfully logged out.');;
 });
+
 Route::get('/admin', function()
 {
     return View::make('admin.index');
 });
 
-Route::post('/login', 'UserController@processLogin');
+Route::get('/admin/blog/{page}', function()
+{
+    return View::make('blog');
+});
+
+Route::get('/admin/tutorials/{page}', function()
+{
+    return View::make('tutorials');
+});
+
+Route::get('/admin/projects/{page}', function()
+{
+    return View::make('projects');
+});
--- app/views/admin/index.blade.php
+++ app/views/admin/index.blade.php
@@ -4,19 +4,31 @@
 <div id="viewport">
     @include('layout.header')
     <div id="main">
+    <section class="admin-nav">
+        <ul class="quicknav">
+            <li class="brdr"><span class="fa fa-quote-left"></span><strong>Blog</strong>
+            <li class="brdr"><a href="admin/blog/view">View</a>
+            <li class="brdr"><a href="admin/blog/create">Create</a>
+            <li><a href="admin/blog/edit">Edit</a>
+            <li class="sect brdr"><span class="fa fa-graduation-cap"></span><strong>Tutorials</strong>
+            <li class="brdr"><a href="admin/tutorials/view">View</a>
+            <li class="brdr"><a href="admin/tutorials/create">Create</a>
+            <li><a href="admin/tutorials/edit">Edit</a>
+            <li class="sect brdr"><span class="fa fa-rocket"></span><strong>Projects</strong>
+            <li class="brdr"><a href="admin/projects/view">View</a>
+            <li class="brdr"><a href="admin/projects/create">Create</a>
+            <li><a href="admin/projects/edit">Edit</a>
+        </ul>
+    </section>
     <section>
         <div class="subsection left-aligned">
-            <h2>Quick navigation.</h2>
-            <ul class="quicknav">
-                <li>View blogs
-                <li>Create blog
-                <li>--
-                <li>View tutorials
-                <li>Create tutorails
-                <li>--
-                <li>View projects
-                <li>Create projects
-            </ul>
+            <h2>Something here.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+            proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class="subsection left-aligned">
             <h2>Some more information</h2>
